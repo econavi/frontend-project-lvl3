78ddc8f6347b4874730afd07a13d097439834f26
Vladimir Kochenov <kochenov.vl@gmail.com>
2023-04-23 07:58:36 +0300

    Add i18next config

---
+++ src/i18next.js
@@ -0,0 +1,21 @@
+import i18next from 'i18next'
+
+i18next.init({
+  lng: 'ru',
+  resources: {
+    ru: {
+      translation: {
+        urlIncorrect: 'Ссылка должна быть валидным URL',
+        urlExist: 'RSS уже существует',
+        rssUploaded: 'RSS успешно загружен',
+        notEmpty: 'Не должно быть пустым',
+        networkError: 'Ошибка сети',
+        loading: 'Загрузка...'
+      }
+    }
+  }
+})
+
+const useTranslate = () => ({ t: i18next.t })
+
+export default useTranslate
