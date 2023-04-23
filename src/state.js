5f85c16f9b440de424188a8ba423abfa20cd71ed
Vladimir Kochenov <kochenov.vl@gmail.com>
2023-04-23 08:04:21 +0300

    Add initial state

---
+++ src/state.js
@@ -0,0 +1,29 @@
+import onChange from 'on-change'
+
+const getInitialState = (handler) => {
+  const state = onChange(
+    {
+      form: {
+        process: 'filling',
+        inputValue: '',
+        error: ''
+      },
+      catalog: {
+        feeds: [],
+        posts: [],
+        readedPosts: new Set()
+      },
+      modal: {
+        openedPost: null
+      }
+    },
+    (path, value) => handler(path, value, state),
+    {
+      isShallow: false
+    }
+  )
+
+  return state
+}
+
+export default getInitialState
