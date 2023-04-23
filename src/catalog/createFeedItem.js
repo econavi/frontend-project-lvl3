b967dedf89fe92265e482d745d002f9e91c6d763
Vladimir Kochenov <kochenov.vl@gmail.com>
2023-04-23 08:03:31 +0300

    Create catalog

---
+++ src/catalog/createFeedItem.js
@@ -0,0 +1,20 @@
+function createFeedItem(feedData) {
+  const { title, description } = feedData
+
+  const li = document.createElement('li')
+  const h3 = document.createElement('h3')
+  const p = document.createElement('p')
+
+  li.setAttribute('class', 'list-group-item border-0 border-end-0')
+  h3.setAttribute('class', 'h6 m-0')
+  p.setAttribute('class', 'm-0 small text-black-50')
+
+  h3.textContent = title
+  p.textContent = description
+
+  li.append(h3, p)
+
+  return li
+}
+
+export default createFeedItem
---
+++ src/catalog/createPostItem.js
@@ -0,0 +1,30 @@
+const createPostItem = (postData, readedPosts, onClick) => {
+  const { id, title, link } = postData
+  const isReaded = readedPosts.has(id)
+
+  const li = document.createElement('li')
+
+  li.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-start border-0 border-end-0')
+
+  const a = document.createElement('a')
+  a.setAttribute('href', link)
+  a.setAttribute('target', '_blank')
+  a.setAttribute('class', isReaded ? 'fw-normal' : 'fw-bold')
+  a.dataset.id = id
+  a.textContent = title
+
+  const button = document.createElement('button')
+  button.setAttribute('type', 'button')
+  button.setAttribute('class', 'btn btn-outline-primary btn-sm')
+  button.dataset.id = id
+  button.dataset.bsToggle = 'modal'
+  button.dataset.bsTarget = '#modal'
+  button.textContent = 'Просмотр'
+  button.addEventListener('click', onClick(id))
+
+  li.append(a, button)
+
+  return li
+}
+
+export default createPostItem
